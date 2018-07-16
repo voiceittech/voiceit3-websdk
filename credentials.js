2cdb8bc2b8de2f33ee1a50ab74c897befdf8676a
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2018-07-16 12:23:07 -0500

    Converted Credentials.js to use Environment Variables

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
-        API_TOKEN: 'API_KEY_HERE',
-        API_KEY: 'TOKEN_HERE'
+        API_TOKEN: process.env.VOICEIT_TWO_API_KEY,
+        API_KEY: process.env.VOICEIT_TWO_API_TOKEN
 }
