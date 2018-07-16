8c501e7be0cddd5371d10061b59cc2846233766a
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2018-07-16 11:57:14 -0500

    Updated credentials.js

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
-        API_TOKEN: 'key_c5da31551e064a438d42f133f59e4b9d',
-        API_KEY: 'tok_3f3fb627f33b435a8995f417c824be66'
+        API_TOKEN: 'API_KEY_HERE',
+        API_KEY: 'TOKEN_HERE'
 }
