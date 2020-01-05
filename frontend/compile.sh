66d1bd6951f95769b172c7c67d2dba578efca6df
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2020-01-05 03:35:49 -0600

    fix frontend (and build scripts to build frontend

--- frontend/compile.sh
+++
@@ -1,4 +0,0 @@
-yarn build
-cp dist/voiceit2.min.js ../dist/voiceit2.min.js
-cp dist/voiceit2.min.js ../node-example/public/js/voiceit2.min.js
-cp dist/voiceit2.min.js ../php-example/js/voiceit2.min.js
