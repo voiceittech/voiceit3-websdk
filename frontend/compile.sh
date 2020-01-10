a8fcf6ea52508ebd47ccffa0ccb3c9703fe3f39f
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2020-01-10 16:01:00 -0600

    Addressed yarn install removed Docker building of voiceit.min.js

---
+++ frontend/compile.sh
@@ -0,0 +1,4 @@
+yarn build
+cp dist/voiceit2.min.js ../dist/voiceit2.min.js
+cp dist/voiceit2.min.js ../node-example/public/js/voiceit2.min.js
+cp dist/voiceit2.min.js ../php-example/js/voiceit2.min.js
