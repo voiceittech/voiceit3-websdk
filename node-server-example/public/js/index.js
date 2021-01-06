3fbe15860caf979de6ecfdaca64e14447d058ab7
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2021-01-06 10:02:36 -0600

    WSDK-21

--- node-server-example/public/js/index.js
+++ node-server-example/public/js/index.js
@@ -1,4 +1,4 @@
-const contentLanguage = "es-ES";
+const contentLanguage = "en-US";
 
 function mobileCheck() {
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
