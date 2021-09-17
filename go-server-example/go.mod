a28b2c73c1ad772eb324a96c9f73f3c6d54f7177
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2021-09-17 10:24:25 -0500

    Initial work on Go Server example

---
+++ go-server-example/go.mod
@@ -0,0 +1,8 @@
+module github.com/voiceittech/VoiceIt2-WebSDK/go-server-example
+
+go 1.17
+
+require (
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/voiceittech/VoiceIt2-WebSDK/voiceit-go-websdk v0.0.0-20210917151955-036bb26c92ba // indirect
+)
---
+++ go-server-example/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	websdk "github.com/voiceittech/VoiceIt2-WebSDK/voiceit-go-websdk"
+)
+
+const (
+	HTTP_PORT = "8080"
+)
+
+func main() {
+
+	r := chi.NewRouter()
+	r.Use(middleware.RealIP)
+	r.Use(middleware.RequestID)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+
+	r.Post("/", websdk.MakeCall)
+
+	FileServer(r)
+
+	fmt.Println("Serving VoiceIt WebSDK Example at port :" + HTTP_PORT)
+	log.Fatal(http.ListenAndServe(":"+HTTP_PORT, r))
+}
+
+func FileServer(router *chi.Mux) {
+	root := "public/"
+	fs := http.FileServer(http.Dir(root))
+
+	router.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		if _, err := os.Stat(root + r.RequestURI); os.IsNotExist(err) {
+			http.StripPrefix(r.RequestURI, fs).ServeHTTP(w, r)
+		} else {
+			fs.ServeHTTP(w, r)
+		}
+	})
+}
