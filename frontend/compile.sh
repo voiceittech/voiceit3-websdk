736c8178c452edc2dc9eb3a586e583282eff91c2
Noel Grover <159802846+noel-enquanta@users.noreply.github.com>
2021-09-17 13:31:26 -0500

    Remove generated license file in compile.sh script

--- frontend/compile.sh
+++ frontend/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 npm run build
+rm dist/voiceit2.min.js.LICENSE.txt
 cp dist/voiceit2.min.js ../dist/voiceit2.min.js
 cp dist/voiceit2.min.js ../node-server-example/public/js/voiceit2.min.js
 cp dist/voiceit2.min.js ../php-server-example/js/voiceit2.min.js
